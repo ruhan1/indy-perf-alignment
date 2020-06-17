e9c0a000435d6770152399992b30eb87b2708770
Rui Han <ruhan@redhat.com>
2020-06-17 15:56:36 +0800

    support url to fetch artifacts

--- Jenkinsfile
+++ Jenkinsfile
@@ -12,19 +12,18 @@ pipeline {
                     def userInput = input message: "Please enter a test suite to run:",
                     parameters:[
                         string(name: 'indyUrl', defaultValue: 'http://indy-perf-nos-automation.cloud.paas.psi.redhat.com/api/content/maven/group/DA-temporary-builds', description: 'Indy group URL to test'),
-                        string(name: 'artifacts', defaultValue: 'artifacts', description: 'artifacts(275), activemq-artemis(76), camel(634), etc'),
-                        string(name: 'artifactsCount', defaultValue: '10', description: 'max artifacts count to retrieve')
+                        string(name: 'artifacts', defaultValue: 'camel', description: 'artifacts(275), activemq-artemis(76), camel(634), etc. Or you can specify a repour log url to fetch artifacts from, e.g., http://orch.psi.redhat.com/pnc-rest/rest/build-records/53917/repour-log.'),
+                        string(name: 'limit', defaultValue: '10', description: 'artifacts limit')
                     ]
                     env.indyUrl = userInput.indyUrl
                     env.artifacts = userInput.artifacts
-                    env.artifactsCount = userInput.artifactsCount
+                    env.limit = userInput.limit
                 }
             }
         }
         stage('Build') {
             steps {
-                //sh "echo ${env.indyUrl}"
-                sh "mvn clean test -DindyUrl=${env.indyUrl} -Dartifacts=${env.artifacts} -DartifactsCount=${env.artifactsCount}"
+                sh "mvn clean test -DindyUrl=${env.indyUrl} -Dartifacts=${env.artifacts} -Dlimit=${env.limit}"
             }
         }
     }
--- src/test/java/org/commonjava/indy/tools/perf/MetadataRetrievalTest.java
+++ src/test/java/org/commonjava/indy/tools/perf/MetadataRetrievalTest.java
@@ -30,6 +30,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -40,6 +41,7 @@ import java.util.stream.Collectors;
 
 import static java.lang.Integer.min;
 import static java.lang.System.currentTimeMillis;
+import static java.util.Collections.emptySet;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 import static org.codehaus.plexus.util.StringUtils.isNotBlank;
 
@@ -48,29 +50,27 @@ public class MetadataRetrievalTest
 {
     private static final int timeout = new Long( TimeUnit.SECONDS.toMillis( 300 ) ).intValue();
 
-    private static final String INDY_URL =
-                    "http://indy-admin-stage.psi.redhat.com/api/content/maven/group/DA-temporary-builds";
-
     @Test
     public void test() throws Exception
     {
-        String indyUrl = System.getProperty( "indyUrl", INDY_URL ); // -DindyUrl
+        String indyUrl = System.getProperty( "indyUrl" ); // -DindyUrl
         System.out.println( "Use indyUrl: " + indyUrl );
 
-        String artifacts = System.getProperty( "artifacts", "artifacts" );
+        // Get artifacts from 1. built-in file, 2. url, e.g., http://orch.psi.redhat.com/pnc-rest/rest/build-records/53917/repour-log
+        String artifacts = System.getProperty( "artifacts" );
         System.out.println( "Use artifacts: " + artifacts );
 
-        int artifactsCount =
-                        Integer.parseInt( System.getProperty( "artifactsCount", "10" ) ); // e.g., -DartifactsCount=20
-        System.out.println( "Use artifactsCount: " + artifactsCount );
+        int limit = Integer.parseInt( System.getProperty( "limit", "10" ) ); // e.g., -Dlimit=20
+        System.out.println( "Use limit: " + limit );
 
-        Set<String> artifactSet = getArtifacts( artifactsCount, artifacts );
+        HttpClient client = getHttpClient();
+
+        Set<String> artifactSet = getArtifacts( limit, artifacts, client );
+        System.out.println( "Get artifacts, size: " + artifactSet.size() );
 
         Set<String> metadataPaths = artifactSet.stream()
                                                .map( artifact -> getMetadataPath( artifact ) )
                                                .collect( Collectors.toSet() );
-
-        HttpClient client = getHttpClient();
 
         long begin = currentTimeMillis();
         System.out.println( "Starts: " + new Date( begin ) );
@@ -88,13 +88,25 @@ public class MetadataRetrievalTest
 
     }
 
-    private Set<String> getArtifacts( int artifactsCount, String resource ) throws IOException
+    private Set<String> getArtifacts( int limit, String resource, HttpClient client ) throws IOException
+    {
+        if ( resource.startsWith( "http://" ) )
+        {
+            return fetchArtifactsFromUrl( resource, limit, client );
+        }
+        else
+        {
+            return fetchArtifactsFromResource( resource, limit );
+        }
+    }
+
+    private Set<String> fetchArtifactsFromResource( String resource, int limit ) throws IOException
     {
         Set<String> artifacts = new HashSet<>();
         try (InputStream in = getClass().getClassLoader().getResourceAsStream( resource ))
         {
             String[] lines = IOUtil.toString( in ).split( "\n" );
-            for ( int i = 0; i < min( artifactsCount, lines.length ); i++ )
+            for ( int i = 0; i < min( limit, lines.length ); i++ )
             {
                 String line = lines[i];
                 if ( isNotBlank( line ) && !line.startsWith( "#" ) )
@@ -109,6 +121,45 @@ public class MetadataRetrievalTest
             }
         }
         return artifacts;
+    }
+
+    private Set<String> fetchArtifactsFromUrl( String url, int limit, HttpClient client ) throws IOException
+    {
+        HttpGet request = new HttpGet( url );
+        HttpResponse response = client.execute( request );
+        StatusLine sl = response.getStatusLine();
+        String content;
+        if ( sl.getStatusCode() == 200 )
+        {
+            content = entityToString( response );
+        }
+        else
+        {
+            System.out.println( "Failed to fetch artifacts, url: " + url );
+            return emptySet();
+        }
+
+        String[] lines = content.split( "\\n" );
+        String artifactsLine = null;
+        for ( String line : lines )
+        {
+            int index = line.indexOf( "REST Client returned" );
+            if ( index > 0 )
+            {
+                artifactsLine = line.substring( line.indexOf( "{" ) + 1, line.indexOf( "}" ) );
+                break;
+            }
+        }
+
+        if ( artifactsLine == null )
+        {
+            System.out.println( "Not find the line" );
+            return emptySet();
+        }
+
+        return Arrays.stream( artifactsLine.split( "," ) )
+                     .map( s -> s.substring( 0, s.indexOf( "=" ) ) )
+                     .collect( Collectors.toSet() );
     }
 
     private HttpClient getHttpClient()
